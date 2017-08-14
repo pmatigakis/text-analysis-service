b945f6fffdfbbd59ba1401a9eed27e29d4bc4e76
Panagiotis Matigakis <pmatigakis@gmail.com>
2017-08-14 20:53:15 +0300

    Removed the built-in support for sentry. It is better to configure it using the LOGGING configuration variable

--- tas/application.py
+++ tas/application.py
@@ -1,7 +1,6 @@
 import logging.config
 
 from falcon import API
-from raven.handlers.logging import SentryHandler
 
 from tas.configuration.loaders import Configuration
 from tas.routes import load_resources
@@ -14,23 +13,12 @@ def _setup_logging(configuration):
         logging.config.dictConfig(log_config)
 
 
-def _initialize_sentry(configuration):
-    handler = SentryHandler(configuration["SENTRY_DSN"])
-    handler.setLevel(configuration["SENTRY_LOG_LEVEL"])
-
-    logger = logging.getLogger("tas")
-    logger.addHandler(handler)
-
-
 def create_app(settings_file):
     configuration = Configuration.load_from_py(settings_file)
 
     app = API()
 
     _setup_logging(configuration)
-
-    if configuration["SENTRY_DSN"]:
-        _initialize_sentry(configuration)
 
     load_resources(configuration, app)
 
