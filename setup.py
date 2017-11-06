8da8689fd1b56819e16f625d0baf3c0b3231ffb3
Panagiotis Matigakis <pmatigakis@gmail.com>
2017-11-06 21:46:10 +0200

    The package requirements are listed in the requirement files

--- setup.py
+++ setup.py
@@ -1,27 +1,37 @@
 from setuptools import setup, find_packages
 
+
+def get_requirements():
+    with open("requirements.txt") as f:
+        requirements = [
+            line.strip()
+            for line in f
+            if not line.startswith("-e")
+        ]
+
+    return requirements
+
+
+def get_test_requirements():
+    with open("requirements-test.txt") as f:
+        requirements = [
+            line.strip()
+            for line in f
+        ]
+
+    return requirements
+
+
 setup(
     name="Text analysis service",
     version="0.3.0",
     packages=find_packages(exclude=["tests"]),
     include_package_data=True,
     zip_safe=False,
-    install_requires=[
-        "falcon==1.1.0",
-        "beautifulsoup4==4.5.1",
-        "python-consul==0.7.0",
-        "gunicorn==19.7.1",
-        "Sphinx==1.6.2",
-        "newspaper3k==0.2.2",
-        "statsd==3.2.1",
-        "jsonschema==2.6.0"
-    ],
+    install_requires=get_requirements(),
     test_suite="nose.collector",
-    tests_require=[
-        "nose==1.3.7",
-        "requests==2.12.4"
-    ],
-    entry_points = {
+    tests_require=get_test_requirements(),
+    entry_points={
         'console_scripts': ['tas-cli=tas.cli:main'],
     }
 )
